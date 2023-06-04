e47b2174c289cfa3c168db5b359ab99bda54e216
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-06-04 18:54:20 +0900

    init directory management system project

---
+++ Directory Management System Project/src/Personnel.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Personnel {
+	public ArrayList<Person> personList;
+
+	public Personnel() {
+		personList = new ArrayList<Person>();
+	}
+
+	public void addPersonnel(Person p) {
+		personList.add(p);
+	}
+}
---
+++ Directory Management System Project/src/totalObjects.java
@@ -0,0 +1,15 @@
+public class totalObjects {
+   private static int numObjects = 0;
+
+   public totalObjects() {
+      numObjects = 0;
+   }
+
+   public void objectAdded() {
+      numObjects++;
+   }
+
+   public int getTotalObjects() {
+      return numObjects;
+   }
+}
